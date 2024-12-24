de35560029d2dde46ea85b0cf65edc8bf8273d06
Sandra ter Maat <sandra97termaat@hotmail.nl>
2024-12-24 10:26:59 +0100

    day 9b

--- src/day8/solution.rs
+++ src/day8/solution.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use itertools::Itertools;
 
@@ -11,7 +11,7 @@ pub fn solve() {
     print!("Nr of antinodes: {}", nr_of_antinodes);
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone, Hash)]
 struct Pos {
     row: isize,
     col: isize,
@@ -32,10 +32,10 @@ fn place_antinodes(antennas: Vec<Antenna>, map: &Vec<Vec<char>>) -> u64 {
         .into_iter()
         .into_grouping_map_by(|antenna| antenna.frequency)
         .collect();
-    let connections = find_connections(antennas_hashmap, map);
-    // let nr_of_connections = connections.len() as u64;
+    // let connections = find_connections(antennas_hashmap, map);
+    let connections = find_connections_with_resonant_harmonics(antennas_hashmap, map);
     // let mut new_map = map.clone();
-    // for connection in connections {
+    // for connection in &connections {
     //     new_map[connection.row as usize][connection.col as usize] = '#';
     // }
     // print_map(&new_map);
@@ -65,6 +65,56 @@ fn find_connections(
         }
     });
     connections
+}
+
+fn find_connections_with_resonant_harmonics(
+    antennas_hashmap: HashMap<char, Vec<Antenna>>,
+    map: &Vec<Vec<char>>,
+) -> Vec<Pos> {
+    let mut connections = HashSet::new();
+    antennas_hashmap.values().for_each(|antennas| {
+        for antenna in antennas {
+            for other_antenna in antennas {
+                if antenna.pos != other_antenna.pos {
+                    let new_connections = get_antinodes_with_resonant_harmonics(
+                        &antenna.pos,
+                        &other_antenna.pos,
+                        map,
+                    );
+                    for connection in new_connections {
+                        connections.insert(connection);
+                    }
+                }
+            }
+        }
+    });
+    connections.into_iter().collect()
+}
+
+fn get_antinodes_with_resonant_harmonics(
+    current_antenna: &Pos,
+    other_antenna: &Pos,
+    map: &Vec<Vec<char>>,
+) -> Vec<Pos> {
+    let mut antinodes = vec![];
+    let width = map[0].len();
+    let height = map.len();
+    let delta_x = other_antenna.col - current_antenna.col;
+    let delta_y = other_antenna.row - current_antenna.row;
+    let mut antinode_pos = Pos {
+        row: other_antenna.row,
+        col: other_antenna.col,
+    };
+    while (antinode_pos.row < height as isize && antinode_pos.col < width as isize)
+        && (antinode_pos.row >= 0 && antinode_pos.col >= 0)
+    {
+        antinodes.push(antinode_pos);
+        antinode_pos = Pos {
+            row: antinode_pos.row + delta_y,
+            col: antinode_pos.col + delta_x,
+        };
+    }
+    antinodes
 }
 
 fn get_antinode_pos(
