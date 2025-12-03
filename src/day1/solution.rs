8e2adf881d8a4baac77e2564b262903387d9c794
Sandra <sandra97termaat@hotmail.nl>
2025-12-03 22:56:04 +0100

    day 1 completed

--- src/day1/solution.rs
+++ src/day1/solution.rs
@@ -2,10 +2,9 @@
 pub fn solve() {
     const INPUT: &str = include_str!("puzzle_input");
     const TEST_INPUT: &str = include_str!("test_input");
-    let instructions = create_list_from_input(TEST_INPUT);
-    print!("instrictions: {:?}", instructions);
+    let instructions = create_list_from_input(INPUT);
     let solution = count_zero(instructions);
-    print!("{}", solution);
+    println!("count {}", solution);
     // calc_distance(list1, list2);
     // calc_similarity(list1, list2);
 }
@@ -38,27 +37,41 @@ fn create_list_from_input(input: &str) -> Vec<Instruction> {
     list
 }
 
-fn rotate_dial(current_pos: i32, instruction: Instruction) -> (bool, i32) {
-    let mut on_zero: bool = false;
+fn rotate_dial(current_pos: i32, instruction: Instruction) -> (u32, i32) {
+    // println!("current pos = {}, next instruction: {:?}", current_pos, instruction);
+    let mut on_zero: u32 = 0;
+    on_zero += instruction.amount as u32 / 100;
     let amount_stripped = instruction.amount % 100;
+    // println!{"Curret pos: {}, ïnstruction: {:?}", current_pos, instruction};
+
     match instruction.direction {
         Direction::Left => {
             let mut next_pos = current_pos - amount_stripped;
-            if next_pos <= 0 {
+            if next_pos < 0 {
+                if current_pos != 0{
+                    on_zero += 1;
+                }
                 next_pos  = 100 + next_pos;
+                // println!{"on zero: {}, next_pos {}", on_zero, next_pos};
             }
-            if next_pos == 0{
-                on_zero = true;
+            if next_pos == 0 || next_pos == 100{
+                on_zero += 1;
+               // println!{"on zero: {}, next_pos {}", on_zero, next_pos};
             }
             (on_zero, next_pos)
         }
         Direction::Right => {
             let mut next_pos = current_pos + amount_stripped;
-            if next_pos >= 100 {
+            if next_pos > 100 {
+                if current_pos != 100{
+                    on_zero += 1;
+                }
                 next_pos = next_pos - 100;
+             //   println!{"on zero: {}, next_pos {}", on_zero, next_pos};
             }
-            if next_pos == 0{
-                on_zero = true;
+            if next_pos == 0 || next_pos == 100{
+                on_zero += 1;
+            //    println!{"on zero: {}, next_pos {}", on_zero, next_pos};
             }
             (on_zero, next_pos)
         }
@@ -67,11 +80,11 @@ fn rotate_dial(current_pos: i32, instruction: Instruction) -> (bool, i32) {
 
 fn count_zero(instructions: Vec<Instruction>) -> u32{
     let mut count = 0;
-    let mut pos = 0;
+    let mut pos = 50;
     for instruction in instructions {
         let state = rotate_dial(pos, instruction);
         pos = state.1;
-        count = count + state.0 as u32;
+        count = count + state.0;
     }
     count
 }
