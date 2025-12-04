4b0230b9b508bef0b72f02c667553d55b62a33f0
Sandra <sandra97termaat@hotmail.nl>
2025-12-04 19:53:45 +0100

    wip 2b

--- src/day2/solution.rs
+++ src/day2/solution.rs
@@ -1,8 +1,10 @@
+use itertools::Itertools;
+
 pub fn solve() {
     const INPUT: &str = include_str!("puzzle_input");
     const TEST_INPUT: &str = include_str!("./test_input");
-    let parsed_input = input_to_ranges(INPUT);
-    let invalid_ids = check_valid(parsed_input);
+    let parsed_input = input_to_ranges(TEST_INPUT);
+    let invalid_ids = split_in_equal_pieces_and_check_if_valid(parsed_input);
 }
 
 #[derive(Debug)]
@@ -35,6 +37,42 @@ fn check_valid(ranges: Vec<Range>) -> usize {
     let sum = invaled_ids.into_iter().sum();
     println!("sum is {}", sum);
     sum
+}
+
+fn split_in_equal_pieces_and_check_if_valid(ranges: Vec<Range>) -> usize {
+    let mut invalid_ids: Vec<usize> = Vec::new();
+    let prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+    for range in ranges {
+        for i in range.start..range.end {
+            let i_as_str = i.to_string();
+            let i_len = i_as_str.len();
+            for prime_number in prime_numbers {
+                if i_len % prime_number == 0 {
+                    let chunks = i_as_str
+                        .chars()
+                        .chunks(i_len / prime_number)
+                        .into_iter()
+                        .map(|chunk| chunk.into_iter().map(|c| c.to_string()).collect::<String>())
+                        .collect::<Vec<String>>();
+                    if all_same(&chunks) {
+                        invalid_ids.push(i);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    let sum = invalid_ids.into_iter().sum();
+    println!("sum is {}", sum);
+    sum
+}
+fn all_same<T: PartialEq>(v: &[T]) -> bool {
+    // Handle empty or single-element vectors
+    if v.len() < 2 {
+        return true;
+    }
+    // Check if all elements are equal to the first element
+    v.iter().all(|x| x == &v[0])
 }
 
 fn is_symmetrical(i: usize) -> bool {
